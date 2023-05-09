a68d47def145b24038d83f78915968a924e270b5
yoondy22 <def314@yonsei.ac.kr>
2023-05-09 15:44:44 +0900

    2023.05.09 홈화면 추가

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ size = [680 + 200, 680]  # 게임창 크기 [w, h]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Yoon's Omok")  # 게임창 이름
 
-done = False
 stone_board = [[0 for j in range(15)] for i in range(15)]  # 오목판 15*15 배열, 0=돌없음, 1=흑돌, 2=백돌
 board_stack = [copy.deepcopy(stone_board)]
 grid_size = 40  # 격자 한 칸의 가로세로 픽셀
@@ -24,6 +23,238 @@ winner = 0  # 1=흑, 2=백
 game_end = False
 x, y = -1, -1
 grid_origin_x, grid_origin_y = 60, 60
+
+
+def start_screen():
+    done = False
+
+    screen.fill((255, 242, 214))
+    # background_img = pygame.image.load("background2.png")
+    # background_img = pygame.transform.scale(background_img, (880, 680))
+    # screen.blit(background_img, (0, 0))
+
+    title_font = pygame.font.SysFont("arial", 50, True, True)  # 제목 폰트
+
+    title_text = title_font.render("OMOK GAME HAGI", True, BLACK)
+    title_text_rect = title_text.get_rect()
+    title_text_rect.center = (440, 100)  # 제목 위치
+    screen.blit(title_text, title_text_rect)
+
+    menu_font = pygame.font.SysFont("arial", 35, True, True)  # 1player, 2player, quit 폰트
+
+    pygame.draw.rect(screen, BLACK, [310, 200, 260, 70], 0)  # 1player black
+    player1_text = menu_font.render("1 player", True, WHITE)
+    player1_text_rect = player1_text.get_rect()
+    player1_text_rect.center = (440, 235)
+    screen.blit(player1_text, player1_text_rect)
+
+    pygame.draw.rect(screen, WHITE, [310, 300, 260, 70], 0)  # 1player white
+    player1_text = menu_font.render("1 player", True, BLACK)
+    player1_text_rect = player1_text.get_rect()
+    player1_text_rect.center = (440, 335)
+    screen.blit(player1_text, player1_text_rect)
+
+    pygame.draw.rect(screen, (150, 150, 150), [310, 400, 260, 70], 0)  # 2player
+    player2_text = menu_font.render("2 player", True, WHITE)
+    player2_text_rect = player2_text.get_rect()
+    player2_text_rect.center = (440, 435)
+    screen.blit(player2_text, player2_text_rect)
+
+    pygame.draw.rect(screen, BLACK, [360, 550, 160, 70], 2)  # quit
+    quit_text = menu_font.render("Q U I T", True, BLACK)
+    quit_text_rect = quit_text.get_rect()
+    quit_text_rect.center = (440, 585)
+    screen.blit(quit_text, quit_text_rect)
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                mouse_pos = pygame.mouse.get_pos()
+                if 310 <= mouse_pos[0] <= 569 and 400 <= mouse_pos[1] <= 469:  # 2 player
+                    player2_mode()
+                    return
+
+                if 360 <= mouse_pos[0] <= 519 and 500 <= mouse_pos[1] <= 569:  # quit
+                    done = True
+
+        pygame.display.flip()
+
+    pygame.quit()
+    quit()
+
+
+def player2_mode():
+    global order, full_order
+    global winner
+    global x, y
+
+    done = False
+
+    while not done:
+        draw_board()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # 닫기 버튼 누르면 게임창 종료
+                done = True
+
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # 마우스 클릭 & 좌클릭
+                mouse_pos = pygame.mouse.get_pos()
+
+                if not game_end and 40 <= mouse_pos[0] <= 639 and 40 <= mouse_pos[1] <= 639:
+                    x, y = (mouse_pos[0] - 40) // 40, (mouse_pos[1] - 40) // 40
+                    if order % 2:
+                        put_white(x, y)
+                    else:
+                        put_black(x, y)
+                    winner = check_omok(x, y)
+
+                elif 700 <= mouse_pos[0] <= 769 and 410 <= mouse_pos[1] <= 479:
+                    undo()
+
+                elif 790 <= mouse_pos[0] <= 859 and 410 <= mouse_pos[1] <= 479:
+                    redo()
+                    winner = check_omok(x, y)
+
+                elif 700 <= mouse_pos[0] <= 769 and 500 <= mouse_pos[1] <= 569:
+                    undo_all()
+
+                elif 790 <= mouse_pos[0] <= 859 and 500 <= mouse_pos[1] <= 569:
+                    redo_all()
+                    winner = check_omok(x, y)
+
+                elif 700 <= mouse_pos[0] <= 859 and 590 <= mouse_pos[1] <= 659:
+                    for i in range(full_order):
+                        board_stack.pop()
+                    order, full_order = 0, 0
+                    start_screen()
+
+        pygame.display.flip()
+
+    pygame.quit()
+    quit()
+
+
+def draw_board():
+    global game_end
+
+    screen.fill(WHITE)  # Background color
+    pygame.time.Clock().tick(60)  # FPS
+
+    # background_img = pygame.image.load("background.webp")
+    # background_img = pygame.transform.scale(background_img, (880, 680))
+    # screen.blit(background_img, (0, 0))
+    pygame.draw.rect(screen, (247, 201, 122), [20, 20, 640, 640], 0)  # 좌측 오목판
+    pygame.draw.rect(screen, (221, 221, 221), [680, 0, 200, 680], 0)  # 우측 메뉴판
+
+    # 격자
+    for i in range(14):
+        for j in range(14):
+            pygame.draw.rect(screen, BLACK,
+                             [grid_origin_x + grid_size * i, grid_origin_y + grid_size * j, grid_size, grid_size], 1)
+    pygame.draw.rect(screen, BLACK, [59, 59, 562, 562], 1)
+
+    for i in range(15):
+        for j in range(15):
+            if board_stack[order][j][i] == 1:
+                pygame.draw.circle(screen, BLACK, [grid_origin_x + grid_size * i, grid_origin_y + grid_size * j], stone_size, 0)
+            if board_stack[order][j][i] == 2:
+                pygame.draw.circle(screen, WHITE, [grid_origin_x + grid_size * i, grid_origin_y + grid_size * j], stone_size, 0)
+                pygame.draw.circle(screen, BLACK, [grid_origin_x + grid_size * i, grid_origin_y + grid_size * j], stone_size, 1)
+
+    sequence_control_font = pygame.font.SysFont("arial", 15, True, False)
+
+    pygame.draw.rect(screen, (190, 255, 190), [700, 410, 70, 70], 0)
+    undo_text = sequence_control_font.render("Undo", True, BLACK)
+    undo_text_rect = undo_text.get_rect()
+    undo_text_rect.center = (735, 445)
+    screen.blit(undo_text, undo_text_rect)
+
+    pygame.draw.rect(screen, (190, 255, 190), [790, 410, 70, 70], 0)
+    redo_text = sequence_control_font.render("Redo", True, BLACK)
+    redo_text_rect = redo_text.get_rect()
+    redo_text_rect.center = (825, 445)
+    screen.blit(redo_text, redo_text_rect)
+
+    pygame.draw.rect(screen, (190, 255, 190), [700, 500, 70, 70], 0)
+    undo_all_text = sequence_control_font.render("Undo All", True, BLACK)
+    undo_all_text_rect = undo_all_text.get_rect()
+    undo_all_text_rect.center = (735, 535)
+    screen.blit(undo_all_text, undo_all_text_rect)
+
+    pygame.draw.rect(screen, (190, 255, 190), [790, 500, 70, 70], 0)
+    redo_all_text = sequence_control_font.render("Redo All", True, BLACK)
+    redo_all_text_rect = redo_all_text.get_rect()
+    redo_all_text_rect.center = (825, 535)
+    screen.blit(redo_all_text, redo_all_text_rect)
+
+    quit_font = pygame.font.SysFont("arial", 30, True, False)
+
+    pygame.draw.rect(screen, WHITE, [700, 590, 160, 70], 0)
+    pygame.draw.rect(screen, BLACK, [700, 590, 160, 70], 3)
+    undo_text = quit_font.render("H O M E", True, BLACK)
+    undo_text_rect = undo_text.get_rect()
+    undo_text_rect.center = (780, 625)
+    screen.blit(undo_text, undo_text_rect)
+
+    if winner == 1:
+        font = pygame.font.SysFont("arial", 50, True, False)
+        winning_text = font.render("BLACK WIN!", True, RED)
+        screen.blit(winning_text, (190, 310))
+        game_end = True
+
+    if winner == 2:
+        font = pygame.font.SysFont("arial", 50, True, False)
+        winning_text = font.render("WHITE WIN!", True, RED)
+        screen.blit(winning_text, (200, 310))
+        game_end = True
+
+
+def put_black(x, y):  # 검은돌 착수
+    global stone_board
+    global order
+    global full_order
+
+    stone_board = copy.deepcopy(board_stack[order])
+
+    if stone_board[y][x]:  # 돌이 이미 존재하면
+        return  # 착수 불가
+
+    put_stone_sound = pygame.mixer.Sound("put_stone.mp3")
+    put_stone_sound.play()
+
+    stone_board[y][x] = 1
+
+    for i in range(full_order - order):
+        board_stack.pop()
+
+    order += 1
+    full_order = order
+    board_stack.append(copy.deepcopy(stone_board))
+
+
+def put_white(x, y):
+    global stone_board
+    global order
+    global full_order
+
+    stone_board = copy.deepcopy(board_stack[order])
+
+    if stone_board[y][x]:
+        return
+
+    put_stone_sound = pygame.mixer.Sound("put_stone.mp3")
+    put_stone_sound.play()
+
+    stone_board[y][x] = 2
+
+    for i in range(full_order - order):
+        board_stack.pop()
+
+    order += 1
+    full_order = order
+    board_stack.append(copy.deepcopy(stone_board))
 
 
 def check_omok(x, y):
@@ -91,44 +322,6 @@ def check_omok(x, y):
     return 0
 
 
-def put_black(x, y):  # 검은돌 착수
-    global order
-    global full_order
-
-    stone_board = copy.deepcopy(board_stack[order])
-
-    if stone_board[y][x]:  # 돌이 이미 존재하면
-        return  # 착수 불가
-
-    stone_board[y][x] = 1
-
-    for i in range(full_order - order):
-        board_stack.pop()
-
-    order += 1
-    full_order = order
-    board_stack.append(copy.deepcopy(stone_board))
-
-
-def put_white(x, y):
-    global order
-    global full_order
-
-    stone_board = copy.deepcopy(board_stack[order])
-
-    if stone_board[y][x]:
-        return
-
-    stone_board[y][x] = 2
-
-    for i in range(full_order - order):
-        board_stack.pop()
-
-    order += 1
-    full_order = order
-    board_stack.append(copy.deepcopy(stone_board))
-
-
 def undo():
     global order
     global winner
@@ -159,113 +352,5 @@ def redo_all():
     order = full_order
 
 
-def draw_board():
-    global game_end
-
-    screen.fill(WHITE)  # Background color
-    pygame.time.Clock().tick(60)  # FPS
-
-    pygame.draw.rect(screen, (247, 201, 122), [20, 20, 640, 640], 0)  # 좌측 오목판
-    pygame.draw.rect(screen, (221, 221, 221), [680, 0, 200, 680], 0)  # 우측 메뉴판
-
-    # 격자
-    for i in range(14):
-        for j in range(14):
-            pygame.draw.rect(screen, BLACK,
-                             [grid_origin_x + grid_size * i, grid_origin_y + grid_size * j, grid_size, grid_size], 1)
-    pygame.draw.rect(screen, BLACK, [59, 59, 562, 562], 1)
-
-    for i in range(15):
-        for j in range(15):
-            if board_stack[order][j][i] == 1:
-                pygame.draw.circle(screen, BLACK, [grid_origin_x + grid_size * i, grid_origin_y + grid_size * j], stone_size, 0)
-            if board_stack[order][j][i] == 2:
-                pygame.draw.circle(screen, WHITE, [grid_origin_x + grid_size * i, grid_origin_y + grid_size * j], stone_size, 0)
-                pygame.draw.circle(screen, BLACK, [grid_origin_x + grid_size * i, grid_origin_y + grid_size * j], stone_size, 1)
-
-    sequence_control_font = pygame.font.SysFont("arial", 15, True, False)
-
-    pygame.draw.rect(screen, (190, 255, 190), [700, 410, 70, 70], 0)
-    undo_text = sequence_control_font.render("Undo", True, BLACK)
-    undo_text_rect = undo_text.get_rect()
-    undo_text_rect.center = (735, 445)
-    screen.blit(undo_text, undo_text_rect)
-
-    pygame.draw.rect(screen, (190, 255, 190), [790, 410, 70, 70], 0)
-    undo_text = sequence_control_font.render("Redo", True, BLACK)
-    undo_text_rect = undo_text.get_rect()
-    undo_text_rect.center = (825, 445)
-    screen.blit(undo_text, undo_text_rect)
-
-    pygame.draw.rect(screen, (190, 255, 190), [700, 500, 70, 70], 0)
-    undo_text = sequence_control_font.render("Undo All", True, BLACK)
-    undo_text_rect = undo_text.get_rect()
-    undo_text_rect.center = (735, 535)
-    screen.blit(undo_text, undo_text_rect)
-
-    pygame.draw.rect(screen, (190, 255, 190), [790, 500, 70, 70], 0)
-    undo_text = sequence_control_font.render("Redo All", True, BLACK)
-    undo_text_rect = undo_text.get_rect()
-    undo_text_rect.center = (825, 535)
-    screen.blit(undo_text, undo_text_rect)
-
-    quit_font = pygame.font.SysFont("arial", 30, True, False)
-
-    pygame.draw.rect(screen, WHITE, [700, 590, 160, 70], 0)
-    pygame.draw.rect(screen, BLACK, [700, 590, 160, 70], 3)
-    undo_text = quit_font.render("Q U I T", True, BLACK)
-    undo_text_rect = undo_text.get_rect()
-    undo_text_rect.center = (780, 625)
-    screen.blit(undo_text, undo_text_rect)
-
-    if winner == 1:
-        font = pygame.font.SysFont("arial", 50, True, False)
-        winning_text = font.render("BLACK WIN!", True, RED)
-        screen.blit(winning_text, (190, 310))
-        game_end = True
-
-    if winner == 2:
-        font = pygame.font.SysFont("arial", 50, True, False)
-        winning_text = font.render("WHITE WIN!", True, RED)
-        screen.blit(winning_text, (200, 310))
-        game_end = True
-
-
 if __name__ == "__main__":
-    while not done:
-        draw_board()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:  # 닫기 버튼 누르면 게임창 종료
-                done = True
-
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # 마우스 클릭 & 좌클릭
-                mouse_pos = pygame.mouse.get_pos()
-
-                if not game_end and 40 <= mouse_pos[0] <= 639 and 40 <= mouse_pos[1] <= 639:
-                    x, y = (mouse_pos[0] - 40) // 40, (mouse_pos[1] - 40) // 40
-                    if order % 2:
-                        put_white(x, y)
-                    else:
-                        put_black(x, y)
-                    winner = check_omok(x, y)
-
-                elif 700 <= mouse_pos[0] <= 769 and 410 <= mouse_pos[1] <= 479:
-                    undo()
-
-                elif 790 <= mouse_pos[0] <= 859 and 410 <= mouse_pos[1] <= 479:
-                    redo()
-                    winner = check_omok(x, y)
-
-                elif 700 <= mouse_pos[0] <= 769 and 500 <= mouse_pos[1] <= 569:
-                    undo_all()
-
-                elif 790 <= mouse_pos[0] <= 859 and 500 <= mouse_pos[1] <= 569:
-                    redo_all()
-                    winner = check_omok(x, y)
-
-                elif 700 <= mouse_pos[0] <= 859 and 590 <= mouse_pos[1] <= 659:
-                    done = True
-
-        pygame.display.flip()
-
-    pygame.quit()
+    start_screen()
